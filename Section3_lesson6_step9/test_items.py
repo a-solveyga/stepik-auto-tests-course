fea2c82c25a113f9811d32bef1715bf6396f982f
a-solveyga <79803925+a-solveyga@users.noreply.github.com>
2021-05-09 12:18:42 +0300

    commit time sleep

--- Section3_lesson6_step9/test_items.py
+++ Section3_lesson6_step9/test_items.py
@@ -4,6 +4,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_add_to_card_button_is_displayed(browser):
     browser.get(link)
+    # time.sleep(30) #just delete # for 2nd step
     output1 = browser.find_elements_by_css_selector("button.btn-add-to-basket")
     assert output1, "Button isn't displayed"
-    time.sleep(30)
