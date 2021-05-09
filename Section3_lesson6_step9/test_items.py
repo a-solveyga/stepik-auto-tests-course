3e04d7f31dce506ff67fa2214b7c505739ba83d1
a-solveyga <79803925+a-solveyga@users.noreply.github.com>
2021-05-09 11:22:45 +0300

    Update test_items.py

--- Section3_lesson6_step9/test_items.py
+++ Section3_lesson6_step9/test_items.py
@@ -6,4 +6,4 @@ def test_add_to_card_button_is_displayed(browser):
     browser.get(link)
     output1 = browser.find_elements_by_css_selector("button.btn-add-to-basket")
     assert output1, "Button isn't displayed"
-    time.sleep(10)
+    time.sleep(30)
