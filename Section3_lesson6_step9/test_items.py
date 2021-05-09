1e7fe12abbeaded0618ccc12033f58f96b9e4b96
Solveyga Azbite <solveyga.azbite@semrush.com>
2021-05-09 11:14:13 +0300

    add test_items.py

---
+++ Section3_lesson6_step9/test_items.py
@@ -0,0 +1,9 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_add_to_card_button_is_displayed(browser):
+    browser.get(link)
+    output1 = browser.find_elements_by_css_selector("button.btn-add-to-basket")
+    assert output1, "Button isn't displayed"
+    time.sleep(10)
